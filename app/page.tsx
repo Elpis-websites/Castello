3cbaf6c1acb9af047b5209e7140b8a7bd1f7033d
simonequattromini <squattromini@travelsoftware.it>
2026-09-15 22:42:52 +0200

    init-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Home() { 
+  const larghezza = 10 * 10
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert h-5 w-[100px]"
+          className="dark:invert h-5"
           src="/next.svg"
           alt="Next.js logo"
-          width={100}
+          width={larghezza}
           height={20}
           priority
         />
